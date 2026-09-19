d8c49a4b82b9797ec74b931498674f4fe77a6c32
Brian Hickey <bhickey777@gmail.com>
2026-09-19 18:26:51 -0500

    test: added playright test cases
    Added test cases for testing e2e login for reporting

---
+++ hello-world-rpt/e2e/login.spec.ts
@@ -0,0 +1,43 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Reporting App Login', () => {
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5200/login');
+  });
+
+  test('should display the login page', async ({ page }) => {
+
+    await expect(page).toHaveURL(/login/);
+
+    await expect(
+      page.getByRole('heading', { name: /login/i })
+    ).toBeVisible();
+  });
+
+  test('should login and navigate to home page', async ({ page }) => {
+
+    await page.getByLabel('Employee Name').fill('John Doe');
+
+    await page.getByLabel('Password').fill('mission123');
+
+    await page.getByRole('button', {
+      name: /login/i
+    }).click();
+
+    await expect(page).toHaveURL(/home/);
+
+    await expect(
+      page.getByText('Customer Lookup')
+    ).toBeVisible();
+
+    await expect(
+      page.getByText('Employee Information')
+    ).toBeVisible();
+
+    await expect(
+      page.getByText('Reports')
+    ).toBeVisible();
+  });
+
+});
